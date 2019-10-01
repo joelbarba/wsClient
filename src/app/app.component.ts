590cc2121c36c4a73481eab2a9fb3994f0b41043
joel <joel.barba@blueface.com>
2019-10-01 09:57:03 +0100

    Genesis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {webSocket, WebSocketSubject} from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'wsClient';
+  public wsUrl = 'ws://localhost:8001';
+  public status = 0; // 0=disconnected, 1=connected
+  public ws$: WebSocketSubject<any>;
+  public logs: Array<{ time, dir, msg }> = [];
+  public msgToSend = `{ command: 'login', data: { user: 'joel' }}`;
+
+  constructor() {}
+
+  connectWS() {
+    this.ws$ = webSocket(this.wsUrl); // https://rxjs-dev.firebaseapp.com/api/webSocket/webSocket
+    this.ws$.subscribe(
+      msg => {
+        console.log(msg);
+        this.addLog('IN', msg);
+      },
+      err => {
+        this.addLog('ERR', 'Connection lost ' + err);
+        this.disconnectWS();
+      }
+    );
+    console.log('Client Connected');
+    this.status = 1;
+    this.addLog('-', 'Connected');
+  }
+
+  disconnectWS() {
+    if (this.ws$) {
+      this.ws$.complete();
+      console.log('Client Disconnected');
+      this.addLog('-', 'Disconnected');
+    }
+    this.status = 0;
+  }
+
+  sendMsg() {
+    if (this.ws$ && this.status === 1) {
+      this.addLog('OUT', this.msgToSend);
+      this.ws$.next(this.msgToSend);
+    }
+  }
+
+  addLog(dir, msg) {
+    const now = new Date();
+    const time = now.toDateString() + ' ' + now.toTimeString().split('GMT')[0];
+    this.logs.unshift({ time, dir, msg });
+  }
+
+
 }
